8666fa78115df35006d655dcd0da74049d06bb6d
mdtoufiqulislam7 <mdtoufiq83@gmail.com>
2025-10-10 00:31:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
